f5b3ef572c48984f389af64801af75a8c05bd7a4
mesheni <bunin.ivan14@yandex.ru>
2025-03-02 16:07:16 +0300

    Добавлено: - добавлен файл русской локализации - удалена нацистская реплика

--- app/src/main/java/ua/acclorite/book_story/ui/about/AboutBadges.kt
+++ app/src/main/java/ua/acclorite/book_story/ui/about/AboutBadges.kt
@@ -6,60 +6,11 @@
 
 package ua.acclorite.book_story.ui.about
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import ua.acclorite.book_story.R
 import ua.acclorite.book_story.presentation.about.AboutEvent
-import ua.acclorite.book_story.ui.about.data.AboutData
-import ua.acclorite.book_story.ui.common.helpers.showToast
 
 @Composable
 fun AboutBadges(
     navigateToBrowserPage: (AboutEvent.OnNavigateToBrowserPage) -> Unit
 ) {
-    val context = LocalContext.current
-
-    Box(
-        modifier = Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.Center
-    ) {
-        LazyRow(
-            Modifier.padding(18.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(13.dp)
-        ) {
-            items(
-                AboutData.badges,
-                key = { it.id }
-            ) { badge ->
-                AboutBadgeItem(badge = badge) {
-                    when (badge.id) {
-                        "tryzub" -> {
-                            context.getString(R.string.slava_ukraini)
-                                .showToast(context = context, longToast = false)
-                        }
-
-                        else -> {
-                            badge.url?.let {
-                                navigateToBrowserPage(
-                                    AboutEvent.OnNavigateToBrowserPage(
-                                        page = it
-                                    )
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
